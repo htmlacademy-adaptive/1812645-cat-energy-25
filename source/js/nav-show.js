a62f99fa8d7fde774bc5a3059a8259f882b0852a
lgmarch <lmarch@yandex.ru>
2022-05-24 02:49:11 +0300

    Верстка index.html, catalog.html.

--- source/js/nav-show.js
+++ source/js/nav-show.js
@@ -1,7 +1,6 @@
-let navigation = document.querySelector('.main-header__nav');
+let navigation = document.querySelector('.nav__list');
 let nav_btn = document.querySelector('.logo__menu-button');
 
 nav_btn.addEventListener('click', function() {
   navigation.classList.toggle('nav--show');
-  navigation.classList.toggle('nav--hidden');
 });
