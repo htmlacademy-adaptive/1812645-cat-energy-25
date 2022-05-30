e1a7b7808e02491394670ca3b8998cb7c97dd9e7
lgmarch <lmarch@yandex.ru>
2022-05-30 16:20:42 +0300

    Исправление по Б24, Б5, Б30, Б17.

--- source/js/nav-show.js
+++ source/js/nav-show.js
@@ -2,6 +2,8 @@ let navigation = document.querySelector('.main-header');
 let nav_btn = document.querySelector('.main-header__button');
 
 navigation.classList.remove('main-header--nojs');
+navigation.classList.remove('main-header--opened');
+navigation.classList.add('main-header--closed');
 
 nav_btn.addEventListener('click', function() {
   navigation.classList.toggle('main-header--opened');
